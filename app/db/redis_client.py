1ad6ea22b7121aec33a59e7330ca4c3b72a0beb7
danilstenkin <danilstenkin57@gmail.com>
2025-06-10 12:14:56 +0500

    feat: [save][clear][set]_lead_state for interaction with Redis

--- app/db/redis_client.py
+++ app/db/redis_client.py
@@ -1,10 +1,27 @@
 # app/db/redis_client.py
 import redis.asyncio as redis
 import os
+import json
 
 redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST", "127.0.0.1"),
     port=int(os.getenv("REDIS_PORT", 6379)),
     db=0,
-    decode_responses=True  # чтобы возвращать строки, а не байты
+    decode_responses=True
 )
+
+REDIS_TTL = 60 * 60 * 24 * 30  # 30 дней
+
+async def get_lead_state(phone: str) -> str | None:
+    data = await redis_client.get(phone)
+    print("📦 ДАННЫЕ ИЗ REDIS:", data)
+    return data  # Просто строка, без json.loads
+
+async def save_lead_state(phone: str):
+    await redis_client.set(phone, "NEW", ex=REDIS_TTL)
+
+async def clear_lead_state(phone: str):
+    await redis_client.delete(phone)
+
+async def set_lead_state(phone: str, state: str):
+    await redis_client.set(phone, state)
