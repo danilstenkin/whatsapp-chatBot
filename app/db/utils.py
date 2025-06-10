b248d1a2cae33f34841b5c408f79b7c38f3e5d52
danilstenkin <danilstenkin57@gmail.com>
2025-06-10 12:20:05 +0500

    feat: save_client_FirstName

--- app/db/utils.py
+++ app/db/utils.py
@@ -4,3 +4,12 @@ async def has_client_first_name(phone: str) -> bool:
     query = "SELECT first_name FROM clients WHERE phone = :phone"
     row = await db.fetch_one(query=query, values={"phone": phone})
     return bool(row and row["first_name"])
+
+async def save_client_first_name(phone: str, first_name: str):
+    query = """
+    UPDATE clients
+    SET first_name = :first_name
+    WHERE phone = :phone
+    """
+    result = await db.execute(query=query, values={"phone": phone, "first_name": first_name})
+    return result
